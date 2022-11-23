3a9e876563d764d7f7a974700c36be12cd634c8a
Travis <travisliu708@gmail.com>
2022-11-23 15:26:13 -0800

    change player client layout

---
+++ player-client/src/components/Layout.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+type Props ={
+  children: React.ReactNode,
+}
+
+
+export default function Layout({children}:Props){
+  return(
+    <div className="bg-grey h-screen">
+      {children}
+    </div>
+  )
+}
--- player-client/src/index.tsx
+++ player-client/src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import PlayerClient from './PlayerClient';
+import Layout from './components/Layout';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   // <React.StrictMode>
+  <Layout>
+
     <PlayerClient />
+  </Layout>
   // </React.StrictMode>
 );
 
