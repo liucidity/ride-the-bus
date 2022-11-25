4ff0cd15c2763b8ede7b008c9c623356a5800aed
Travis <travisliu708@gmail.com>
2022-11-24 18:43:49 -0800

    remove headers and credentials

--- server/express.js
+++ server/express.js
@@ -10,8 +10,8 @@ const io = socketIo(server, {
   cors: {
     origin: ["http://localhost:4000", "http://localhost:3000", 'https://ride-the-bus-player.onrender.com/', 'https://ride-the-bus.onrender.com/'],
     // methods: ['GET', 'POST']
-    // allowedHeaders: ["controller"],
-    // credentials: true,
+    allowedHeaders: ["Access-Control-Allow-Origin"],
+    credentials: true,
   },
 }); //in case server and client run on different urls
 
