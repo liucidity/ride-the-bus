f058a3afbf1d147cea77ed534fcbe96832d0f309
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-11-24 17:12:00 -0800

    fix spacing issue with the chat and new choice messages

--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -2,19 +2,23 @@ import { statSync } from "fs"
 import React from "react"
 
 type Props={
-  status: any,
+  state: any,
 }
 
-export default function Message({ status }:Props) {
+export default function Message({ state }:Props) {
+
+  const playerGuesses = Object.keys(state.players).map((player) => {
+    return {player, choice: state.players[player].choice}
+  })
+
   return (
     <div id='message-box'>
-      {status === 'correct' && <h1>You guessed right! Move onto the next round.</h1>}
-      {status === 'incorrect' && <h1>You guessed wrong! Take a shot and restart.</h1>}
-      {status.gameState === 'end' && <h1>{status.winner} has won! 🏆</h1>}
-      {status === 1 && <h1>Guess if the first card has a red suit or black suit...</h1>}
-      {status === 2 && <h1>Guess if the second card is higher or lower than the first card...</h1>}
-      {status === 3 && <h1>Guess if the third card is between or outside the first two cards...</h1>}
-      {status === 4 && <h1>Guess the suit of the fourth card...</h1>}
+      {state.status === 'reveal' && playerGuesses.map((player) => <h6>{player.player} picked: {player.choice}</h6>)}
+      {state.gameState === 'end' && <h1>{state.winner} has won! 🏆</h1>}
+      {state.status === 'none' && state.round === 1 && <h1>Guess if the first card has a red suit or black suit...</h1>}
+      {state.status === 'none' && state.round === 2 && <h1>Guess if the second card is higher or lower than the first card...</h1>}
+      {state.status === 'none' && state.round === 3 && <h1>Guess if the third card is between or outside the first two cards...</h1>}
+      {state.status === 'none' && state.round === 4 && <h1>Guess the suit of the fourth card...</h1>}
     </div>
   )
 }
